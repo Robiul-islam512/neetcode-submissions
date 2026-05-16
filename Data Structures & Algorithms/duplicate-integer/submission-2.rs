f54ed55ae6bff345b494f78149e8a31895390ef1
Robiul islam <151074623+Robiul-islam512@users.noreply.github.com>
2026-05-16 14:52:10 +0600

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        let mut freqCount:HashMap<i32,i32> = HashMap::new();
+
+        for val in nums{
+            freqCount.entry(val).and_modify(|count|*count+=1).or_insert(1);
+            if *freqCount.get(&val).unwrap()>=2{
+                return true
+            }
+        }
+        false
+    }
+}
